6e9fcc9614ce0496e937d95edccdc5e48c51dfa8
Aaliyan <aliyanmuhammad840@gmail.com>
2023-09-23 00:08:50 +0500

    code update

--- backend/admin/add-student.php
+++ backend/admin/add-student.php
@@ -63,14 +63,14 @@
                                          <div class="col-12 col-sm-4">
                                              <div class="form-group local-forms">
                                                  <label> Name <span class="login-danger">*</span></label>
-                                                 <input name="name" class="form-control" type="text" placeholder="Enter Your Name">
+                                                 <input id="name" name="name" pattern="^[A-Za-z\s'-]+$" class="form-control" required type="text" placeholder="Enter Your Name">
                                              </div>
                                          </div>
 
                                          <div class="col-12 col-sm-4">
                                              <div class="form-group local-forms">
                                                  <label>Gender <span class="login-danger">*</span></label>
-                                                 <select name="gender" class="form-control select">
+                                                 <select required name="gender" class="form-control select">
                                                      <option>Select Gender</option>
                                                      <option value="male">Male</option>
                                                      <option value="female">Female</option>
@@ -81,19 +81,19 @@
                                          <div class="col-12 col-sm-4">
                                              <div class="form-group local-forms calendar-icon">
                                                  <label>Date Of Birth <span class="login-danger">*</span></label>
-                                                 <input name="dob" class="form-control datetimepicker" type="text" placeholder="DD-MM-YYYY">
+                                                 <input required name="dob" class="form-control datetimepicker" type="text" placeholder="DD-MM-YYYY">
                                              </div>
                                          </div>
                                          <div class="col-12 col-sm-4">
                                              <div class="form-group local-forms">
                                                  <label>Roll </label>
-                                                 <input name="roll" class="form-control" type="text" placeholder="Enter Roll Number">
+                                                 <input required name="roll" class="form-control" type="text" placeholder="Enter Roll Number">
                                              </div>
                                          </div>
                                          <div class="col-12 col-sm-4">
                                              <div class="form-group local-forms">
                                                  <label>Religion <span class="login-danger">*</span></label>
-                                                 <select name="religion" class="form-control select">
+                                                 <select required name="religion" class="form-control select">
                                                      <option>Please Select Religion </option>
                                                      <option value="islam">Islam</option>
                                                      <option value="christian">Christian</option>
@@ -104,25 +104,25 @@
                                          <div class="col-12 col-sm-4">
                                              <div class="form-group local-forms">
                                                  <label>E-Mail <span class="login-danger">*</span></label>
-                                                 <input name="email" class="form-control" type="email" placeholder="Enter Email Address">
+                                                 <input required name="email" class="form-control" type="email" placeholder="Enter Email Address">
                                              </div>
                                          </div>
                                          <div class="col-12 col-sm-4">
                                              <div class="form-group local-forms">
                                                  <label>Grade <span class="login-danger">*</span></label>
-                                                 <input name="grade" class="form-control" type="number" min="1" max="10" placeholder="Enter Class">
+                                                 <input required name="grade" class="form-control" type="number" min="1" max="10" placeholder="Enter Class">
                                              </div>
                                          </div>
                                          <div class="col-12 col-sm-4">
                                              <div class="form-group local-forms">
                                                  <label>Phone </label>
-                                                 <input class="form-control" name="phone" type="text" placeholder="Enter Phone Number">
+                                                 <input required class="form-control" name="phone" type="text" placeholder="Enter Phone Number">
                                              </div>
                                          </div>
                                          <div class="col-12 col-sm-4">
                                              <div class="form-group local-forms">
                                                  <label>Age </label>
-                                                 <input class="form-control" name="age" type="number" min="7" max="20" placeholder="Enter Age">
+                                                 <input required class="form-control" name="age" type="number" min="7" max="20" placeholder="Enter Age">
                                              </div>
                                          </div>
 
@@ -139,19 +139,19 @@
                                          <div class="col-12 col-sm-4">
                                              <div class="form-group local-forms">
                                                  <label>Parent Name<span class="login-danger">*</span></label>
-                                                 <input name="parent_name" class="form-control" type="text" placeholder="Enter Parent Name">
+                                                 <input pattern="^[A-Za-z\s'-]+$" required name="parent_name" class="form-control" type="text" placeholder="Enter Parent Name">
                                              </div>
                                          </div>
                                          <div class="col-12 col-sm-4">
                                              <div class="form-group local-forms">
                                                  <label>Parent Email <span class="login-danger">*</span></label>
-                                                 <input name="parent_email" class="form-control" type="email" placeholder="Enter Parent Email">
+                                                 <input required name="parent_email" class="form-control" type="email" placeholder="Enter Parent Email">
                                              </div>
                                          </div>
                                          <div class="col-12 col-sm-4">
                                              <div class="form-group local-forms">
                                                  <label>Parent Phone <span class="login-danger">*</span></label>
-                                                 <input name="parent_phone" class="form-control" type="text" placeholder="Enter Parent Phone No">
+                                                 <input name="parent_phone" required class="form-control" type="text" placeholder="Enter Parent Phone No">
                                              </div>
                                          </div>
 
@@ -171,7 +171,98 @@
 
      </div>
 
-
+     <script>
+         const dob = document.getElementById("dob");
+         const roll = document.getElementById("roll");
+         const age = document.getElementById("age");
+         const phone = document.getElementById("phone");
+         const grade = document.getElementById("grade");
+         const email = document.getElementById("email");
+         const parentPhone = document.getElementById("parent_phone");
+         const parentEmail = document.getElementById("parent_email");
+         const parentInput = document.getElementById("parent_name");
+         const nameInput = document.getElementById("name"); // Replace "name" with your input field's ID
+         const namePattern = /^[A-Za-z\s'-]+$/;
+         const dobpattern = /^\d{4}-\d{2}-\d{2}$/;
+         const rollpattern = /^[A-Za-z0-9]+$/;
+         const agepattern = /^[0-9]{1,3}$/;
+         const phonepattern = /^\d{10}$/;
+         const gradepattern = /^[A-Za-z0-9-+]+$/;
+         const emailpattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+         const parentphonepattern = /^\d{10}$/;
+         const parentemailpattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+         
+         nameInput.addEventListener("input", function() {
+             if (!namePattern.test(nameInput.value)) {
+                 nameInput.setCustomValidity("Please enter a valid name.");
+             } else {
+                 nameInput.setCustomValidity("");
+             }
+         });
+         parentInput.addEventListener("input", function() {
+             if (!namePattern.test(nameInput.value)) {
+                 nameInput.setCustomValidity("Please enter a valid name.");
+             } else {
+                 nameInput.setCustomValidity("");
+             }
+         });
+         email.addEventListener("input", function() {
+             if (!emailpattern.test(email.value)) {
+                email.setCustomValidity("Please enter a valid email.");
+             } else {
+                email.setCustomValidity("");
+             }
+         });
+         phone.addEventListener("input", function() {
+             if (!phonepattern.test(phone.value)) {
+                phone.setCustomValidity("Please enter a valid phone no.");
+             } else {
+                phone.setCustomValidity("");
+             }
+         });
+         parentEmail.addEventListener("input", function() {
+             if (!parentemailpattern.test(parentEmail.value)) {
+                parentEmail.setCustomValidity("Please enter a valid email.");
+             } else {
+                parentEmail.setCustomValidity("");
+             }
+         });
+         parentPhone.addEventListener("input", function() {
+             if (!parentphonepattern.test(parentPhone.value)) {
+                parentPhone.setCustomValidity("Please enter a valid phone no.");
+             } else {
+                parentPhone.setCustomValidity("");
+             }
+         });
+         dob.addEventListener("input", function() {
+             if (!dobpattern.test(dob.value)) {
+                dob.setCustomValidity("Please enter a valid Dob.");
+             } else {
+                dob.setCustomValidity("");
+             }
+         });
+         age.addEventListener("input", function() {
+             if (!agepattern.test(age.value)) {
+                age.setCustomValidity("Please enter a valid age.");
+             } else {
+                age.setCustomValidity("");
+             }
+         });
+         roll.addEventListener("input", function() {
+             if (!rollpattern.test(roll.value)) {
+                roll.setCustomValidity("Please enter a valid name.");
+             } else {
+                roll.setCustomValidity("");
+             }
+         });
+         grade.addEventListener("input", function() {
+             if (!gradepattern.test(grade.value)) {
+                grade.setCustomValidity("Please enter a valid grade.");
+             } else {
+                grade.setCustomValidity("");
+             }
+         });
+     </script>
      <script src="../assets/js/jquery-3.6.0.min.js"></script>
 
      <script src="../assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
